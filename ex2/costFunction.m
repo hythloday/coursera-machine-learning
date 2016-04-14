192aef2406fa2f5ae6d24206fc595938186126ac
R C James Harlow <git@james.harlow.name>
2016-04-15 00:21:24 +0100

    Week 3 assignment complete

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,25 +7,18 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
 
 
+hox = sigmoid(transpose(sum(transpose(transpose(theta) .* X))));
 
+J = sum((-y .* log(hox)) - (1-y) .* log(1 - hox)) / m;
 
-
-
-
+grad = transpose(sum(X .* (hox - y))) ./ m;
 
 % =============================================================
 
--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -2,14 +2,10 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -18,8 +14,11 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+hox = sigmoid(transpose(sum(transpose(transpose(theta) .* X))));
 
+J = (sum((-y .* log(hox)) - (1-y) .* log(1 - hox)) / m) + (lambda * sum(theta(2:end) .* theta(2:end)) / (2 * m));
 
+grad = (transpose(sum(X .* (hox - y))) ./ m) + [0; lambda .* theta(2:end) ./ m ];
 
 
 % =============================================================
